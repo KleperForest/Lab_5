c5526ad52cff6bdb773059369f70f80d2cb1e3b7
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-04-12 00:29:31 -0600

    Title

--- LAB_5/LAB_5/main.c
+++ LAB_5/LAB_5/main.c
@@ -1,10 +1,12 @@
-/*
- * LAB_5.c
- *
- * Created: 4/12/2024 12:27:37 AM
- * Author : samue
- */ 
-
+//*********************************************************************
+// Universidad del Valle de Guatemala
+// IE2023: Programación de Microcontroladores
+// Autor: Alan Gomez
+// Proyecto: LAB_5.c
+// Descripción: Quinto Laboratorio de Programación de Microcontroladores con lenguaje C.
+// Hardware: ATmega328p
+// Created: 4/12/2024 12:27:37 AM
+//*********************************************************************
 #include <avr/io.h>
 
 
