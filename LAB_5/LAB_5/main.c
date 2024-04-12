2d89e60fc05982fb5610fb05ea209e04e0c7eed4
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-04-12 00:27:50 -0600

    Start

---
+++ LAB_5/LAB_5/main.c
@@ -0,0 +1,18 @@
+/*
+ * LAB_5.c
+ *
+ * Created: 4/12/2024 12:27:37 AM
+ * Author : samue
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
