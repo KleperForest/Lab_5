6a12e2e814eef69da1a73284a6ba8cc451dbae7e
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-04-12 16:09:24 -0600

    d

--- LAB_5/LAB_5/main.c
+++ LAB_5/LAB_5/main.c
@@ -25,3 +25,39 @@ int main(void) {
 		setServoAngle(servo_angle);// Mapeo y muestra
 	}
 }
+
+// Codigo sin librerias .c .h servo
+/*//*********************************************************************
+// Universidad del Valle de Guatemala
+// IE2023: Programación de Microcontroladores
+// Autor: Alan Gomez
+// Proyecto: LAB_5.c
+// Descripción: Quinto Laboratorio de Programación de Microcontroladores con lenguaje C.
+// Hardware: ATmega328p
+// Created: 4/12/2024 12:27:37 AM
+//*********************************************************************
+  
+  #include <avr/io.h>
+  #include <util/delay.h>
+
+  int main(void)
+  {
+	  DDRB |= (1<<PB1); // PB1 salida
+
+	  ADMUX |= (1<<REFS0) | (1<<MUX0); // Referencia 5V y usar ADC1
+	  ADCSRA |= (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1); // Enable ADC and set prescaler to 64
+
+	  TCCR1A |= (1<<COM1A1) | (1<<WGM11); // Fast PWM, non-inverting mode
+	  TCCR1B |= (1<<WGM13) | (1<<WGM12) | (1<<CS11); // Fast PWM, prescaler = 8
+	  ICR1=39999;   //20ms PWM period
+
+	  while (1){
+		  ADCSRA |= (1<<ADSC); //Conversion ADC
+		  while (ADCSRA & (1<<ADSC)); // Esperar que la lecura termine
+		  uint16_t servo_pos = ADC * 4.8866;// Mapeo
+		  servo_pos = (servo_pos>=999)? servo_pos : 999;// Evitar que decienda más de 999 ya que es 0 grados
+		  OCR1A = servo_pos;
+		  _delay_ms(100); // Delay para mostrar en servo
+	  }
+  }
+*/
