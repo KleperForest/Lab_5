5dd1f00864a21219416daafe8dcf2a5c966ff77a
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-04-12 15:14:24 -0600

    x

--- LAB_5/LAB_5/main.c
+++ LAB_5/LAB_5/main.c
@@ -10,27 +10,25 @@
   
   #include <avr/io.h>
   #include <util/delay.h>
-  #include "Controller_Servo.h"
-//servo_pos valor del potenciometro
+
   int main(void)
-  {		
-	  DDRB |= (1<<PB1); // PB1 como salida
-	  
+  {
+	  DDRB |= (1<<PB1); // Set PB1 as output
+
+	  ADMUX |= (1<<REFS0) | (1<<MUX0); // Set reference voltage to AVCC and select ADC1 as input
+	  ADCSRA |= (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1); // Enable ADC and set prescaler to 64
+
 	  TCCR1A |= (1<<COM1A1) | (1<<WGM11); // Fast PWM, non-inverting mode
 	  TCCR1B |= (1<<WGM13) | (1<<WGM12) | (1<<CS11); // Fast PWM, prescaler = 8
-	  ICR1=39999;   //20ms PWM periodo
-	   
-	  ADMUX |= (1<<REFS0) | (1<<MUX0); // Set  5V y escoger ADC1 as input
-	  ADCSRA |= (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1); //  prescaler a 64
+	  ICR1=39999;   //20ms PWM period
 
 	  while (1){
-		  ADCSRA |= (1<<ADSC); // Comenzar ADC 
-		  while (ADCSRA & (1<<ADSC)); // Esperar que la lecura termine del ADC
-		  float servo_val = ADC * 4.8866;// Mapeo
-		 
-		  OCR1A = Val_servo_pos(servo_val);
-		  _delay_ms(100); // Delay for servo to reach positivo
-		  
+		  ADCSRA |= (1<<ADSC); // Start ADC conversion
+		  while (ADCSRA & (1<<ADSC)); // Esperar que la lecura termine
+		  uint16_t servo_pos = ADC * 4.8866;// Mapeo
+		  servo_pos = (servo_pos>=999)? servo_pos : 999;// Evitar que decienda más de 999 ya que es 0 grados
+		  OCR1A = servo_pos;
+		  _delay_ms(100); // Delay for servo to reach position
 	  }
   }
 
